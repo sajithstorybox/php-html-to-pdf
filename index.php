5ac2e12757ff4f534982a7cae036ac9ad79c6324
Sajith K <sajith@insidestorybox.com>
2020-08-18 15:09:00 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+require 'vendor/autoload.php';
+
+// reference the Dompdf namespace
+use Dompdf\Dompdf;
+
+// instantiate and use the dompdf class
+$dompdf = new Dompdf();
+$dompdf->set_option('chroot', '/');
+$dompdf->loadHtmlFile('index.html');
+
+// (Optional) Setup the paper size and orientation
+$dompdf->setPaper('A4', 'portrait');
+
+// Render the HTML as PDF
+$dompdf->render();
+
+// Output the generated PDF to Browser
+$dompdf->stream();
